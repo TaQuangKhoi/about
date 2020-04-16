3222e6df5d3b9461bf5793806310f823573ef86b
Tạ Quang Khôi <khoi0941@gmail.com>
2020-04-17 06:20:34 +0700

    add space into script, fix Khoi to Khôi on JVS..

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 ﻿alert
-    ("Đây là gì? là một thông báo, Bạn có muốn thấy trang web không ? Khoi nhớ ngọc à ?..");
+    ("Đây là gì? là một thông báo, Bạn có muốn thấy trang web không ? Khôi nhớ ngọc à ?..");
     
 //nhớ em nhiều, ở trên là học cách viết một thông báo
 //22:27 16 Apr 2020 cannot use ibus-bambo in VS Code
