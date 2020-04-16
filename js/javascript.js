7616483ea542ccbec8881db098e16ba0150e176c
Tạ Quang Khôi <khoi0941@gmail.com>
2020-04-16 22:24:15 +0700

    add info to learnJVS.. change JVS.js content..

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
 ﻿alert
-    ("Đây là gì? là một thông báo, Bạn có muốn thấy trang web không ? nhớ ngọc à ?..");
+    ("Đây là gì? là một thông báo, Bạn có muốn thấy trang web không ? Khoi nhớ ngọc à ?..");
     
 //nhớ em nhiều, ở trên là học cách viết một thông báo 
