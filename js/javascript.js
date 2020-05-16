6caa13e57272301cb2dea124a78dfd7edfd4bfb0
Tạ Quang Khôi <khoi0941@gmail.com>
2020-05-16 14:02:02 +0700

    try twwo morre

--- js/javascript.js
+++ js/javascript.js
@@ -4,17 +4,4 @@
 //nhớ em nhiều, ở trên là học cách viết một thông báo
 //22:27 16 Apr 2020 cannot use ibus-bambo in VS Code
 
-    var n = localStorage.getItem('on_load_counter');
-
-    if (n === null) {
-        n = 0;
-    }
-
-    n++;
-
-    localStorage.setItem("on_load_counter", n);
-nums = n.toString().split('').map(Number);
-document.getElementById('CounterVisitor').innerHTML = '';
-for (var i of nums) {
-  document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';
-}
+ 
