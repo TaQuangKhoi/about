c048392b7a9172c6a09649dbdc4348ce5fd4a3d5
Tạ Quang Khôi <khoi0941@gmail.com>
2020-05-16 13:41:15 +0700

    test new code for count visitors..

--- js/javascript.js
+++ js/javascript.js
@@ -4,36 +4,14 @@
 //nhớ em nhiều, ở trên là học cách viết một thông báo
 //22:27 16 Apr 2020 cannot use ibus-bambo in VS Code
 
-incrementAndShowValue();
+var n = localStorage.getItem('on_load_counter');
 
-function incrementAndShowValue() {
-  var value = getCookie("visitcounter") || 0;
-  var newValue = ("00000" + (Number(value) + 1)).slice(-6);
-  var container = document.getElementById("counterVisitor");
-  String(newValue).split("").forEach(function(item, index) {
-    container.children[index].innerHTML = item;
-  });
-  counter++;
-  setCookie("visitcounter", counter);
-}
+    if (n === null) {
+        n = 0;
+    }
 
-function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + value + expires + "; path=https://stacksnippets.net/js";
-}
+    n++;
 
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
+    localStorage.setItem("on_load_counter", n);
+
+    document.getElementById('CounterVisitor').innerHTML = n;
