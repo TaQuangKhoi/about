6ddef9b5363dfd0ee3598b5e9a1bfe50ae0d00ba
Tạ Quang Khôi <khoi0941@gmail.com>
2020-08-17 15:07:07 +0700

    update my status..

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 ﻿/*alert
     ("này, người trước màn hình có phải Nguyễn Hồng Ngọc không? Đây là gì? là một thông báo, Bạn có muốn thấy trang web không ? Khôi nhớ ngọc à ?..");
     ; */
-//nhớ em nhiều, ở trên là học cách viết một thông báo
-//22:27 16 Apr 2020 cannot use ibus-bambo in VS Code
+// nhớ em nhiều, ở trên là học cách viết một thông báo
+// 22:27 16 Apr 2020 cannot use ibus-bambo in VS Code
 
- 
+// Now i will learning JavaScript with [[freecodecamp.org]]
