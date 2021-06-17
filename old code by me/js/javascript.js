17d79ecd71f3a309db2b42180185f29c3de86abb
Keios-VS2019 <taquangkhoi@hotmail.com>
2021-06-17 21:18:28 +0700

    move all old code to one folder

---
+++ old code by me/js/javascript.js
@@ -0,0 +1,23 @@
+﻿/*alert
+    ("này, người trước màn hình có phải Nguyễn Hồng Ngọc không? Đây là gì? là một thông báo, Bạn có muốn thấy trang web không ? Khôi nhớ ngọc à ?..");
+    ; */
+// nhớ em nhiều, ở trên là học cách viết một thông báo
+// 22:27 16 Apr 2020 cannot use ibus-bambo in VS Code
+
+// Now i will learning JavaScript with [[freecodecamp.org]]
+
+// [[Basic JavaScript: Declare JavaScript Variables]]
+var myName;
+// [[Storing Values with the Assignment Operator]]
+var a;
+var b = 2;
+a = 7;
+// [[Basic JavaScript: Initializing Variables with the Assignment Operator]]
+var c = 3;
+// [[Basic JavaScript: Understanding Uninitialized Variables]]
+a = a + 1;
+a = 6;
+b = b + 5;
+b = 15;
+c = "I am a";
+c = c + " String!";
