e77083247820dc7bef4912d0df510cb718b1294a
Keios-VS2019 <taquangkhoi@hotmail.com>
2021-07-28 16:16:50 +0700

    test 1 - scale to mobile device use solution in StackOverflow

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// function to draw a square
-function drawSquare(x, y, size) {
-    var square = document.getElementById("drawbyMain.JS");
-    square.setAttribute("x", x);
-    square.setAttribute("y", y);
-    square.setAttribute("width", size);
-    square.setAttribute("height", size);
-    square.setAttribute("fill", "red");
-}
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
